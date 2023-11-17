8849517e60cf1ccf7281c8c05a2aa52fb4a8071e
Loay-dahamsheh <dahamshehloay@gmail.com>
2023-11-17 16:48:05 +0200

    db

---
+++ server/Models/donationModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const donationSchema = new mongoose.Schema({
+  donation_title: { type: String, required: true },
+  donation_description: { type: String, required: true },
+  donor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  donation_type: { type: String, required: true },
+  expected_outcome: { type: Number, required: true },
+  created_at: { type: Date, required: true },
+  total_donation: { type: Number, default: 0, required: true },
+  donation_img: { type: String, required: true },
+  is_deleted: { type: Boolean, required: true },
+});
+
+const Donation = mongoose.model('Donation', donationSchema);
+
+module.exports = Donation;
