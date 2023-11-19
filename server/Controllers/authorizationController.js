bf626cdfd2ff4a5a368945fcaea4d0159d31c472
laith17 <laithalmajaly007@gmail.com>
2023-11-19 11:02:05 +0300

    commit

--- server/Controllers/authorizationController.js
+++ server/Controllers/authorizationController.js
@@ -1,89 +1,98 @@
-const User = require('../Models/userModel');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const Joi = require('joi');
+const User = require("../Models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const Joi = require("joi");
 // const Cookies = require('js-cookie');
-require('dotenv').config();
+require("dotenv").config();
 
 const schema = Joi.object({
-    username : Joi.string().alphanum().min(3).max(10).required(),
-    email : Joi.string().email().required(),
-    password : Joi.string().required(),
-    phoneNumber : Joi.string().min(9).max(14).required(),
+  username: Joi.string().min(3).max(30).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  phoneNumber: Joi.string().min(9).max(14).required(),
 });
 
-function validation(username, email, password, phoneNumber){
-const valid = schema.validate({username, email, password, phoneNumber});
-    if (valid.error == undefined){
-        return true;
-    }else {
-        console.log(valid.error);
-        return false;
-    }
-};
+function validation(username, email, password, phoneNumber) {
+  const valid = schema.validate({ username, email, password, phoneNumber });
+  if (valid.error == undefined) {
+    return true;
+  } else {
+    console.log(valid.error);
+    return false;
+  }
+}
 
-async function createUser (req, res){
+async function createUser(req, res) {
   try {
-    const { username, email, password, phoneNumber, age, user_location } = req.body;
+    const { username, email, password, phoneNumber, age, user_location } =
+      req.body;
     const valid = validation(username, email, password, phoneNumber);
-    const serach = await User.findOne({ email : email });
-    if (serach.email){
-        res.status(400).json("this email is already have an account");
-    } else{
-        if (valid){
-            let user_password = await bcrypt.hash(password, 10);
-            const newUser = new User();
-            newUser.username = username;
-            newUser.email = email;
-            newUser.password = user_password;
-            newUser.phoneNumber = phoneNumber;
-            newUser.age = age;
-            newUser.user_location = user_location
-            newUser.save();
-            const accessToken = jwt.sign({id : newUser.id, email : newUser.email, role: newUser.role}, process.env.SECRET_KEY, {expiresIn: '4h'});
-            res.cookie('accessToken', accessToken, { httpOnly: true });
-            res.status(201).json({newUser, accessToken});
-        }else {
-            res.status(400).json("Invalid input");
-        }
+    // const serach = await User.findOne({ email : email });
+    if (false) {
+      res.status(400).json("this email is already have an account");
+    } else {
+      if (valid) {
+        let user_password = await bcrypt.hash(password, 10);
+        const newUser = new User();
+        newUser.username = username;
+        newUser.email = email;
+        newUser.password = user_password;
+        newUser.phoneNumber = phoneNumber;
+        newUser.age = age;
+        newUser.user_location = user_location;
+        newUser.save();
+        const accessToken = jwt.sign(
+          { id: newUser.id, email: newUser.email, role: newUser.role },
+          process.env.SECRET_KEY,
+          { expiresIn: "4h" }
+        );
+        res.cookie("accessToken", accessToken, { httpOnly: true });
+        res.status(201).json({ newUser, accessToken });
+      } else {
+        res.status(400).json("Invalid input");
+      }
     }
   } catch (error) {
-    console.log(error)
-        res.status(500).json({ error: 'Error in user model createUser' });
+    console.log(error);
+    res.status(500).json({ error: "Error in user model createUser" });
   }
-};
+}
 
-async function loginUser (req, res){
-    try {
-      const { email, password } = req.body;
-      const valid = validation("username", email, password, "12345678910");
-      if (valid){
-        const theUser = await User.findOne({ email : email });
-          if (theUser && theUser.email === email) {
-                bcrypt.compare(password , theUser.password, (error, result) => {
-                if (error) {
-                    res.status(400).json(error);
-                } else if (result) {
-                    const accessToken = jwt.sign({id : theUser.id, email : theUser.email, role: newUser.role}, process.env.SECRET_KEY, {expiresIn: '4h'});
-                    res.cookie('accessToken', accessToken, { httpOnly: true });
-                    res.status(200).json({theUser, accessToken});
-                } else {
-                    res.status(400).json('incorrect password');
-                }
-                });
-          }else {
-            res.status(401).json({ error: 'Email not found' });
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+    const valid = validation("username", email, password, "12345678910");
+    if (valid) {
+      const theUser = await User.findOne({ email: email });
+      if (theUser && theUser.email === email) {
+        bcrypt.compare(password, theUser.password, (error, result) => {
+          if (error) {
+            res.status(400).json(error);
+          } else if (result) {
+            const accessToken = jwt.sign(
+              { id: theUser.id, email: theUser.email, role: theUser.role },
+              process.env.SECRET_KEY,
+              { expiresIn: "4h" }
+            );
+            res.cookie("accessToken", accessToken, { httpOnly: true });
+            res.status(200).json({ theUser, accessToken });
+          } else {
+            res.status(400).json("incorrect password");
           }
+        });
       } else {
-            res.status(400).json("Invalid inputs");
+        res.status(401).json({ error: "Email not found" });
       }
-    }catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Email not found' });
+    } else {
+      res.status(400).json("Invalid inputs");
     }
-};
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Email not found" });
+  }
+}
 
 module.exports = {
-    createUser,
-    loginUser
+  createUser,
+  loginUser,
 };
--- server/Routes/donationsRoute.js
+++ server/Routes/donationsRoute.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const donationsController = require("../controllers/donationsController");
+const verifyJWT = require("../Middleware/authorization");
 
-router.post("/addDonation/:donor_id", donationsController.addDonation);
-router.put(
-  "/updateDonation/:donation_id",
-  donationsController.updateDonation
+router.post(
+  "/addDonation",
+  verifyJWT.authorize,
+  donationsController.addDonation
 );
-router.put(
-  "/deleteDonation/:donation_id",
-  donationsController.deleteDonation
-);
+router.put("/updateDonation/:donation_id", donationsController.updateDonation);
+router.put("/deleteDonation/:donation_id", donationsController.deleteDonation);
 router.get("/getDonations", donationsController.getDonations);
-router.get("/filterDonationsByType/:donation_type", donationsController.filterDonationsByType);
 router.get(
-  "/getDonationById/:donation_id",
+  "/filterDonationsByType/:donation_type",
+  donationsController.filterDonationsByType
+);
+router.get(
+  "/getDonationById/:donation_id", 
   donationsController.getDonationById
 );
 
